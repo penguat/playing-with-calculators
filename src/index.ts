f2c8837fc6f27e1a7786f81341220feaad9d103b
Alex / penguat <stream@penguat.co.uk>
2023-04-19 14:16:23 +0100

    Run calculation on input rather than lost focus

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { runToEnd } from "./calculator";
 const textInput = document.getElementById("calculation") as HTMLInputElement;
 const result = document.getElementById("result") as HTMLElement;
 
-textInput.addEventListener("change", () => {
+textInput.addEventListener("input", () => {
     const calculationResult = runToEnd(textInput.value);
     result.innerText = JSON.stringify(calculationResult);
 })
